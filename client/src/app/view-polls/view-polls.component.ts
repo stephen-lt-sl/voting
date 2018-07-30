0b83e5e26e26ee4cd8c69aa821ba4efa6f83ad5d
Stephen Tozer <stozer@scottlogic.co.uk>
2018-07-30 15:57:24 +0100

    Add styling and default mock data to View Polls route

--- client/src/app/view-polls/view-polls.component.ts
+++ client/src/app/view-polls/view-polls.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Poll } from '../models/poll';
 
 @Component({
   selector: 'app-view-polls',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-polls.component.css']
 })
 export class ViewPollsComponent implements OnInit {
+  polls: Poll[] = [
+    { id: 0, question: 'What should we have for lunch?' },
+    { id: 1, question: 'Who is the coolest in the office?' },
+  ];
 
   constructor() { }
 
